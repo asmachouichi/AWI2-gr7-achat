c16ad5aeeadcc0ddd1c4a3ca004777592e7d03b1
chouichiasma2@gmail.com <asmachouichi2@gmail.com>
2025-12-03 14:39:14 +0100

    Modifier le nom du cluster dans variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "cluster_name" {
   description = "Nom du cluster EKS"
   type        = string
-  default     = "my_new_kubernetes"
+  default     = "my_next_kubernetes"
 }
 
 variable "subnet_ids" {
