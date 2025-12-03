d2d0f12ee7290bb1c3e4ca8c7af8b1a2606e2d88
chouichiasma2@gmail.com <asmachouichi2@gmail.com>
2025-12-03 01:35:48 +0100

    Correction des subnets pour EKS, cluster et node group créés

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,24 +13,22 @@ variable "cluster_name" {
 variable "subnet_ids" {
   description = "IDs des sous-réseaux pour le node group"
   type        = list(string)
-  default     = [
-    "subnet-02638865aedfa978a",  # us-east-1f
-    "subnet-076608cfc34c1f733",  # us-east-1d
-    "subnet-01230077daf93a3c6",  # us-east-1c
-    "subnet-0e9d49e4f247fa83d"   # us-east-1b
+ default     = [
+    "subnet-096acffa132c627d1", # us-east-1a
+      "subnet-08de68ae6e83d4f23"  # us-east-1b
   ]
 }
 
 variable "role_arn" {
   description = "ARN du rôle IAM pour EKS"
   type        = string
-  default     = "arn:aws:iam::005777537952:role/LabRole"
+  default     = "arn:aws:iam::905418321314:role/LabRole"
 }
 
 variable "vpc_id" {
   description = "L'ID du VPC pour le cluster EKS"
   type        = string
-  default     = "vpc-0b5bf1513c792fc9b"
+  default     = "vpc-064b2e16500689fc7"
 }
 
 variable "vpc_cidr" {
